9758de014b8d1cb53f4b19d5a73568fbfc0ae775
Ben Meeder <bmeeder22@gmail.com>
2021-08-21 13:06:58 -0500

    Clean up added blogs

--- src/pages/pagesConfig.js
+++ src/pages/pagesConfig.js
@@ -1,13 +1,14 @@
-import jiraSyncLanding from "./jiraSyncLanding.json";
 import jiraSyncLandingYaml from "./jiraSyncLanding.yaml";
-import blog1 from "./blog1.md";
 import yaml from "js-yaml";
 import NotFound from "./404.yaml";
+import Salesforce from "./salesforce.yml";
+import barriers from "./barriers.yml";
 
 let config = {
-  landing1: jiraSyncLanding,
-  landing2: yaml.load(jiraSyncLandingYaml, "utf8"),
-  NotFound: yaml.load(NotFound, "utf8")
+  landing1: yaml.load(jiraSyncLandingYaml, "utf8"),
+  NotFound: yaml.load(NotFound, "utf8"),
+  salesforce: yaml.load(Salesforce, "utf8"),
+  barriers: yaml.load(barriers, "utf8")
 };
 
 export default config;
